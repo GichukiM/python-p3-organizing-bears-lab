33bc120946f570c266d09eda746c7a9155b2208d
Collins Gichuki <gichukimuchiri80@gmail.com>
2024-06-02 20:47:54 +0300

    Complete Lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'Brown', 'Phlegmatic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 10, 'F', 'Black', 'Melancholic', 2);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'Grey', 'Sanguine', 2);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'Dark-Brown', 'Phlegmatic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 15, 'M', 'Black', 'Melancholic', 2);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 11, 'M', 'Grey', 'Phlegmatic', 2);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'F', 'Brown', 'Sanguine', 1);
